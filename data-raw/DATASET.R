6991bde2aae13ebc21d8684e7b3d2593e660b4e0
ChristianGoueguel <christian.goueguel@gmail.com>
2021-09-09 12:18:28 -0400

    Update dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,17 +1,3 @@
 ## code to prepare `specData` dataset
-
-specData <- arrow::read_parquet("~/Documents/Laserag/R&D projects/Soil/Eurofins/SOC/processed data/avg_spec") %>%
-  mutate(
-    calibration = case_when(
-      gdata::startsWith(as.character(sample_id), "SOC") ~ "terraton",
-      TRUE ~ "environex"
-    )
-  ) %>%
-  relocate(calibration, .after = spectra_id) %>%
-  modify_at("calibration", as_factor) %>%
-  filter(calibration %in% "terraton") %>%
-  select(-c(qr_code, calibration, sample_id, reception_date:creation_time)) %>%
-  modify_at("spectra_id", as.character) %>%
-  slice(1:10)
-
+specData <- arrow::read_parquet("~/Documents/Packages/LIBSdata/specData")
 usethis::use_data(specData, overwrite = TRUE)
