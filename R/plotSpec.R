fbb23e98611d4e93c653c2578d7f56e8daa8adb0
ChristianGoueguel <christian.goueguel@gmail.com>
2021-09-12 10:28:22 -0400

    Update in `plotSpec`

--- R/plotSpec.R
+++ R/plotSpec.R
@@ -22,52 +22,80 @@ plotSpec <- function(data, id = NULL, colvar = NULL) {
   globalVariables(names = c("wavelength", "intensity"))
 
   if (is.null(id) == TRUE & is.null(colvar) == TRUE) {
-    data %>%
-      tidyr::pivot_longer(cols = tidyselect::everything(), names_to = "wavelength", values_to = "intensity") %>%
-      purrr::modify_at("wavelength", as.numeric) %>%
-      ggplot2::ggplot(ggplot2::aes_string("wavelength", "intensity")) +
-      ggplot2::geom_line(colour = "darkblue") +
-      ggplot2::labs(x = "Wavelength [nm]", y = "Intensity [arb. units]") +
-      ggplot2::theme_classic() +
-      ggplot2::theme(legend.position = "none", axis.line = ggplot2::element_line(colour = "grey50", size = 1))
+    X <- data %>%
+      tidyr::pivot_longer(
+        cols = tidyselect::everything(),
+        names_to = "wavelength",
+        values_to = "intensity"
+        ) %>%
+      purrr::modify_at("wavelength", as.numeric)
+
+      p <- X %>%
+        ggplot2::ggplot(ggplot2::aes(wavelength, intensity)) +
+        ggplot2::geom_line(colour = "darkblue") +
+        ggplot2::labs(x = "Wavelength [nm]", y = "Intensity [arb. units]") +
+        ggplot2::theme_classic() +
+        ggplot2::theme(legend.position = "none", axis.line = ggplot2::element_line(colour = "grey50", size = 1))
+      return(p)
   }
 
+
   if (is.null(id) == FALSE & is.null(colvar) == TRUE) {
-    data %>%
-      dplyr::select(id) %>%
-      tidyr::pivot_longer(cols = !id, names_to = "wavelength", values_to = "intensity") %>%
-      purrr::modify_at("wavelength", as.numeric) %>%
-      ggplot2::ggplot(ggplot2::aes_string("wavelength", "intensity")) +
+    X <- data %>%
+      tidyr::pivot_longer(
+        cols = !id,
+        names_to = "wavelength",
+        values_to = "intensity"
+        ) %>%
+      purrr::modify_at("wavelength", as.numeric)
+
+    p <- X %>%
+      ggplot2::ggplot(ggplot2::aes(wavelength, intensity)) +
       ggplot2::geom_line(ggplot2::aes(colour = id, group = id)) +
       ggplot2::scale_colour_gradient(low = "blue", high = "red") +
       ggplot2::labs(x = "Wavelength [nm]", y = "Intensity [arb. units]") +
       ggplot2::theme_classic() +
       ggplot2::theme(legend.position = "none", axis.line = ggplot2::element_line(colour = "grey50", size = 1))
+    return(p)
   }
 
+
   if (is.null(id) == TRUE & is.null(colvar) == FALSE) {
-    data %>%
-      dplyr::select(colvar) %>%
-      tidyr::pivot_longer(cols = !colvar, names_to = "wavelength", values_to = "intensity") %>%
-      purrr::modify_at("wavelength", as.numeric) %>%
-      ggplot2::ggplot(ggplot2::aes_string("wavelength", "intensity")) +
+    X <- data %>%
+      tidyr::pivot_longer(
+        cols = !colvar,
+        names_to = "wavelength",
+        values_to = "intensity"
+        ) %>%
+      purrr::modify_at("wavelength", as.numeric)
+
+    p <- X %>%
+      ggplot2::ggplot(ggplot2::aes(wavelength, intensity)) +
       ggplot2::geom_line(ggplot2::aes(colour = colvar, group = colvar)) +
       ggplot2::scale_colour_gradient(low = "blue", high = "red") +
       ggplot2::labs(x = "Wavelength [nm]", y = "Intensity [arb. units]") +
       ggplot2::theme_classic() +
       ggplot2::theme(legend.position = "none", axis.line = ggplot2::element_line(colour = "grey50", size = 1))
+    return(p)
   }
 
   if (is.null(id) == FALSE & is.null(colvar) == FALSE) {
-    data %>%
-      dplyr::select(id, colvar) %>%
-      tidyr::pivot_longer(cols = !c(id, colvar), names_to = "wavelength", values_to = "intensity") %>%
-      purrr::modify_at("wavelength", as.numeric) %>%
-      ggplot2::ggplot(ggplot2::aes_string("wavelength", "intensity")) +
+    X <- data %>%
+      tidyr::pivot_longer(
+        cols = !c(id, colvar),
+        names_to = "wavelength",
+        values_to = "intensity"
+        ) %>%
+      purrr::modify_at("wavelength", as.numeric)
+
+    p <- X %>%
+      ggplot2::ggplot(ggplot2::aes(wavelength, intensity)) +
       ggplot2::geom_line(ggplot2::aes(colour = colvar, group = id)) +
       ggplot2::scale_colour_gradient(low = "blue", high = "red") +
       ggplot2::labs(x = "Wavelength [nm]", y = "Intensity [arb. units]") +
       ggplot2::theme_classic() +
       ggplot2::theme(legend.position = "right", axis.line = ggplot2::element_line(colour = "grey50", size = 1))
+    return(p)
   }
+
 }
