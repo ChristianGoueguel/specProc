6afcfac58345dd56c90fb36bedc691b6cdeb82a3
ChristianGoueguel <christian.goueguel@gmail.com>
2023-03-16 11:33:19 -0700

    Update

--- R/confidence_ellipse.R
+++ R/confidence_ellipse.R
@@ -9,7 +9,6 @@
 #' @return a data frame of the coordinates points of the ellipse.
 #' @export confidence_ellipse
 confidence_ellipse <- function(.data, x = NULL, y = NULL, conf_level = 0.95, by_group = FALSE) {
-
   require(dplyr)
   require(tidyr)
   require(purrr)
@@ -38,7 +37,7 @@ confidence_ellipse <- function(.data, x = NULL, y = NULL, conf_level = 0.95, by_
   if (conf_level < 0 && conf_level > 1) {
     stop("'conf_level' must be between 0 and 1.")
   }
-  if(!is.logical(by_group)) {
+  if (!is.logical(by_group)) {
     stop("'by_group' must be of boolean type (TRUE or FALSE).")
   }
 
@@ -48,9 +47,9 @@ confidence_ellipse <- function(.data, x = NULL, y = NULL, conf_level = 0.95, by_
     cov_mat <- cov(.x)
     eig <- eigen(cov_mat)
 
-    theta <- (2*pi*seq(0, 360, 1))/360
-    B1 <- sqrt(eig$values[1]*qchisq(conf_level, 2)) * cos(theta)
-    B2 <- sqrt(eig$values[2]*qchisq(conf_level, 2)) * sin(theta)
+    theta <- (2 * pi * seq(0, 360, 1)) / 360
+    B1 <- sqrt(eig$values[1] * qchisq(conf_level, 2)) * cos(theta)
+    B2 <- sqrt(eig$values[2] * qchisq(conf_level, 2)) * sin(theta)
 
     R <- cbind(B1, B2) %*% t(eig$vectors)
     C <- R + matrix(rep(t(mean_vec), 361), ncol = ncol(t(mean_vec)), byrow = TRUE)
@@ -60,7 +59,7 @@ confidence_ellipse <- function(.data, x = NULL, y = NULL, conf_level = 0.95, by_
 
   if (by_group == FALSE) {
     X_mat <- .data %>%
-      select({{x}}, {{y}}) %>%
+      select({{ x }}, {{ y }}) %>%
       as.matrix()
 
     Y <- transform_data(X_mat, conf_level)
@@ -78,21 +77,21 @@ confidence_ellipse <- function(.data, x = NULL, y = NULL, conf_level = 0.95, by_
 
     # Group the data by factor columns and nest the data
     nested_data <- df_tbl %>%
-      select({{factor_col}}, {{x}}, {{y}}) %>%
+      select({{ factor_col }}, {{ x }}, {{ y }}) %>%
       nest_by(!!sym(factor_col)) %>%
       ungroup()
 
-    Y <- matrix(0, nrow = 361*length(nested_data$data), ncol = 3)
+    Y <- matrix(0, nrow = 361 * length(nested_data$data), ncol = 3)
 
     for (i in seq_along(nested_data$data)) {
-      group_data <- nested_data%>%
+      group_data <- nested_data %>%
         pluck(2, i) %>%
         select(where(is.numeric)) %>%
         as.matrix()
 
       Y_grp <- transform_data(group_data, conf_level)
       Y_grp <- cbind(Y_grp, replicate(361, nested_data$group[i]))
-      Y[seq(1+(361*(i-1)), 361*i), ] <- Y_grp
+      Y[seq(1 + (361 * (i - 1)), 361 * i), ] <- Y_grp
     }
     Y %<>%
       as_tibble() %>%
@@ -101,5 +100,3 @@ confidence_ellipse <- function(.data, x = NULL, y = NULL, conf_level = 0.95, by_
   }
   return(Y)
 }
-
-
