184d373cafefb59a69227d58735580885f195ac8
ChristianGoueguel <christian.goueguel@gmail.com>
2023-03-15 20:31:16 -0700

    Updates

--- R/adj_boxplot.R
+++ R/adj_boxplot.R
@@ -3,60 +3,58 @@
 #' @source M. Hubert and E. Vandervieren, An adjusted boxplot for skewed distributions, Computational Statistics & Data Analysis, Vol.52, 12, 2008
 #' @param .data data frame or tibble (must contain numeric columns).
 #' @return ggplot2 object.
-#' @export adjusted_boxplot
-adjusted_boxplot <- function(.data) {
+#' @export adj_boxplot
+adj_boxplot <- function(.data) {
+  requireNamespace("robustbase", quietly = TRUE)
+  requireNamespace("ggplot2", quietly = TRUE)
+  requireNamespace("dplyr", quietly = TRUE)
+  requireNamespace("purrr", quietly = TRUE)
 
-  require(robustbase)
-  require(ggplot2)
-  require(dplyr)
-  require(purrr)
-
-  # check input validity
+  # Check input validity
   if (missing(.data)) {
     stop("Missing 'data' argument.")
   }
-  if (!all(.data %>% map_lgl(is.numeric))) {
+  if (!all(.data %>% purrr::map_lgl(is.numeric))) {
     stop("Input must be a numeric data frame.")
   }
 
   # Calculate adjusted boxplot values for each column using tidyverse syntax
   adj_boxplot_vals <- .data %>%
-    map(
+    purrr::map_dfr(
       function(col) {
         adj_box <- robustbase::adjboxStats(col)
-        tibble(
+        tibble::tibble(
           lower = adj_box$stats[1],
           q1 = adj_box$stats[2],
           median = adj_box$stats[3],
           q3 = adj_box$stats[4],
           upper = adj_box$stats[5]
         )
-        }
-      )
+      },
+      .id = "variable"
+    )
 
-  # Convert list to data frame
-  adj_boxplot_tbl <- bind_rows(adj_boxplot_vals, .id = "variable")
-  adj_boxplot_tbl$variable <- factor(adj_boxplot_tbl$variable, levels = names(.data))
+  adj_boxplot_vals$variable <- factor(adj_boxplot_vals$variable, levels = names(.data))
 
   # Create ggplot2 object
-  plot <- ggplot(
-    adj_boxplot_tbl,
-    aes(
+  plot <- ggplot2::ggplot(
+    adj_boxplot_vals,
+    ggplot2::aes(
       x = variable,
       ymin = lower,
       lower = q1,
       middle = median,
       upper = q3,
       ymax = upper
-      )
-    ) +
-    geom_boxplot(stat = "identity") +
-    theme_bw() +
-    labs(
+    )
+  ) +
+    ggplot2::geom_boxplot(stat = "identity") +
+    ggplot2::theme_bw() +
+    ggplot2::labs(
       title = "Adjusted Boxplots for Skewed Distributions",
       x = "Variables",
       y = "Values"
-      )
+    )
 
   return(plot)
 }
