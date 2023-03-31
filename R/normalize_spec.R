ac155cd3997479081c8c40b21dd4559ceed8bb96
ChristianGoueguel <christian.goueguel@gmail.com>
2023-03-31 15:29:38 -0700

    Update

--- R/normalize_spec.R
+++ R/normalize_spec.R
@@ -2,12 +2,13 @@
 #' @author Christian L. Goueguel
 #' @description This function takes a data frame (spectra) as input and normalizes it using one or more specified normalization methods.
 #' @param .data A numeric data frame containing the spectra.
-#' @param method A character vector specifying the normalization method(s) to apply. Available methods are "min_max", "mean", "vector", "area", "max_intensity", "msc", and "snv". Default is "min_max".
+#' @param method A character vector specifying the normalization method(s) to apply. Available methods are "min_max", "mean", "vector", "area", "max_intensity", and "snv". Default is "min_max".
 #' @return A data frame containing the original columns and the normalized columns for each specified method with the method name as a prefix.
 #' @export normalize_spec
 normalize_spec <- function(.data, method = "min_max") {
   requireNamespace("dplyr", quietly = TRUE)
   requireNamespace("purrr", quietly = TRUE)
+  requireNamespace("stats", quietly = TRUE)
 
   # Check input validity
   if (missing(.data)) {
@@ -16,31 +17,30 @@ normalize_spec <- function(.data, method = "min_max") {
   if (!all(.data %>% purrr::map_lgl(is.numeric))) {
     stop("Input must be a numeric data frame.")
   }
-  valid_methods <- c("min_max", "mean", "vector", "area", "max_intensity", "msc", "snv")
+  valid_methods <- c("min_max", "mean", "vector", "area", "max_intensity", "snv")
   if (!all(method %in% valid_methods)) {
-    stop("Invalid method specified. Available methods are: min_max, mean, vector, area, max_intensity, msc, and snv.")
+    stop("Invalid method specified. Available methods are: min_max, mean, vector, area, max_intensity, and snv.")
   }
 
-  res <- .data %>%
-    mutate(across(everything(), as.double)) %>%
-    mutate(
-      across(
-        everything(),
-        list(
-          min_max = ~ ( . - min(.)) / (max(.) - min(.)),
-          mean = ~ (. - mean(.)) / (max(.) - min(.)),
-          vector = ~ . / sqrt(sum(.^2)),
-          area = ~ . / sum(.),
-          max_intensity = ~ . / max(.),
-          # msc = {
-          #   H <- qr.solve(t(.data)) %*% .data;
-          #   ~ . / H[1, ] - H[2, ]
-          # },
-          snv = ~ (. - mean(.)) / sd(.)
-        ),
-        .names = paste0(method, "_", .col)
-      )
-    )
+  if (method == "min_max") {
+    res <- (.data - min(.data)) / (max(.data) - min(.data))
+  }
+  if (method == "mean") {
+    res <- (.data - mean(.data)) / (max(.data) - min(.data))
+  }
+  if (method == "vector") {
+    res <- .data / sqrt(sum(.data^2))
+  }
+  if (method == "area") {
+    res <- .data / sum(.data)
+  }
+  if (method == "max_intensity") {
+    res <- .data / max(.data)
+  }
+  if (method == "snv") {
+    res <- (.data - mean(.data)) / sd(.data)
+  }
 
   return(res)
+
 }
