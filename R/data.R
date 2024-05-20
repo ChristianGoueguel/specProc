7ced24ae726e052c5203932c9b87e815dc1b6fc8
ChristianGoueguel <christian.goueguel@gmail.com>
2024-05-20 13:29:42 -0400

    update

--- R/data.R
+++
@@ -1,7 +0,0 @@
-#' Laser-induced breakdown spectroscopy (LIBS) spectra of 500 soil samples
-#'
-#' A dataset containing the emission spectra of 500 soils measured in laboratory.
-#'
-#' @format A tibble with 10 rows (soil samples) and 15037 variables (wavelength).
-#' @source \doi{10.1039/C9JA00090A}
-"specData"
