d7e42300dd9d4b580ff4fb1a53e8ccf07b97fc16
ChristianGoueguel <christian.goueguel@gmail.com>
2024-05-20 17:13:05 -0400

    Update: Change in `voigt()` using the Faddeeva function and `pracam::erfc()`, instead of the `RccpFaddeeva` pachage which no longer avaliable on CRAN.

--- R/voigt.R
+++ R/voigt.R
@@ -7,11 +7,19 @@
 #' @param wG Gaussian FWHM
 #' @param wL Lorentzian FWHM
 #' @param A area under the peak
+#' @param real logical, return only the real part of the complex Faddeeva
 #' @return fitted value
 #' @export voigt
-voigt <- function(x, y0, xc, wG, wL, A) {
+voigt <- function(x, y0, xc, wG, wL, A, real = TRUE) {
+
+  pVoigt <- function(.x, x0, sigma, gamma, .real = real){
+    z <- (.x - x0 + gamma*1i) / (sigma * sqrt(2))
+    w <- exp(-z^2)*pracma::erfc(-1i*z) # Faddeeva function
+    if(.real) return (Re(w) / (sigma * sqrt(2*pi))) else w / (sigma * sqrt(2*pi))
+  }
+
   s <- wG/(2*sqrt(2*log(2)))
-  y0 + A* RcppFaddeeva::Voigt(x = x, x0 = xc, sigma = s, gamma = wL, real = TRUE)
+  y0 + A* pVoigt(.x = x, x0 = xc, sigma = s, gamma = wL, .real = real)
 }
 
 
