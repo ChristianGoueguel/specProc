4ab19ae124f1bfcbec3a2d7ee6487ad32ac75239
ChristianGoueguel <christian.goueguel@gmail.com>
2024-05-20 13:18:31 -0400

    Updates

--- R/specProc-package.R
+++ R/specProc-package.R
@@ -2,7 +2,18 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom corrr correlate
+#' @importFrom cowplot theme_minimal_vgrid
+#' @importFrom forcats as_factor
+#' @importFrom ggsci scale_fill_d3
 #' @importFrom minpack.lm nls.lm.control
 #' @importFrom minpack.lm nlsLM
+#' @importFrom moments kurtosis
+#' @importFrom moments skewness
+#' @importFrom mt osc_sjoblom
+#' @importFrom mt osc_wise
+#' @importFrom mt osc_wold
+#' @importFrom RcppFaddeeva Voigt
+#' @importFrom ropls opls
 ## usethis namespace: end
 NULL
