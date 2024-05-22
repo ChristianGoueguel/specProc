16741d9bd1d7523024a84b994162b352e2a20fec
ChristianGoueguel <christian.goueguel@gmail.com>
2024-05-21 22:35:00 -0400

    Updates

--- R/biweight_midvariance.R
+++ R/biweight_midvariance.R
@@ -2,8 +2,9 @@
 #' @description This function calculates the biweight midvariance of a numeric vector,
 #'   which is a robust measure of scale that can be used to estimate the variability of the data while being resistant to the influence of outliers.
 #' @details
-#' For scale estimators, the standard deviation (or variance) is the optimal estimator for Gaussian data. However, it is not resistant and it does not have robustness of efficiency. The median absolute deviation (MAD) is a resistant estimate, but it has only modest robustness of efficiency.
-#' The biweight midvariance estimator is both resistant and robust of efficiency. Wilcox (1997), "Introduction to Robust Estimation and Hypothesis Testing," Academic Press.
+#' For scale estimators, the standard deviation (or variance) is the optimal estimator for Gaussian data. However, it is not resistant and it does not have robustness of efficiency.
+#' In robust statistics, the median absolute deviation (MAD) is a resistant estimate, but it has only modest robustness of efficiency, while the biweight midvariance estimator is both resistant and robust of efficiency.
+#' Wilcox (1997), "Introduction to Robust Estimation and Hypothesis Testing," Academic Press.
 #'
 #' @author Christian L. Goueguel
 #' @param X A numeric vector.
@@ -19,12 +20,10 @@ biweight_midvariance <- function(X) {
       stop("Input 'X' must be a numeric vector.")
     }
 
-    tbl <- dplyr::as_tibble(X) %>% dplyr::pull(value)
-
-    beta <- (tbl - stats::median(tbl)) / (9 * stats::qnorm(0.75) * stats::mad(tbl))
+    beta <- (X - stats::median(X)) / (9 * stats::qnorm(0.75) * stats::mad(X))
     alpha <- dplyr::if_else(beta <= -1 | beta >= 1, 0, 1)
-    n <- dplyr::n()
-    nx <- sqrt(n) * sqrt(sum(alpha * (tbl - stats::median(tbl))^2 * (1 - beta^2)^4))
+    n <- nrow(as.matrix(X))
+    nx <- sqrt(n) * sqrt(sum(alpha * (X - stats::median(X))^2 * (1 - beta^2)^4))
     dx <- abs(sum(alpha * (1 - beta^2) * (1 - 5 * beta^2)))
     res <- (nx / dx)^2
 
