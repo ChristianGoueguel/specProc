7b2519adb9a557afbd5c2ef84997706c019dcb6e
ChristianGoueguel <christian.goueguel@gmail.com>
2024-05-24 12:38:26 -0400

    Update formatting

--- R/zscore.R
+++ R/zscore.R
@@ -1,7 +1,9 @@
-#' Classical or Robust Z-Score
+#' @title Classical or Robust Z-Score
 #'
+#' @description
 #' This function is a wrapper around the `robustHD` package and calculates classical or robust z-score (standardization) for a numeric vector.
 #'
+#' @details
 #' Z-scores are useful for comparing data points from different distributions because they are
 #' dimensionless and standardized. A positive z-score indicates that the data point is above the
 #' mean (or the median in the robust approach), while a negative z-score indicates that the data point is below the mean (or the median). One common rule
@@ -9,6 +11,7 @@
 #' z-score greater than 3 (|z| > 3) can be considered potential outliers, as they fall outside the range
 #' that covers 99.7% of the data points in a normal distribution. (Note that a cutoff of |z| > 2.5 is also often used).
 #'
+#' @references
 #' - Rousseeuw, P. J., and Croux, C. (1993). Alternatives to the median absolute deviation.
 #'   Journal of the American Statistical Association, 88(424), 1273-1283.
 #' - Rousseeuw, P. J., and Hubert, M. (2011). Robust statistics for outlier detection.
