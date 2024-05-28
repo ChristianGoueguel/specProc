c62edf1b9b0710c89db0688a1441d971210b2c7d
ChristianGoueguel <christian.goueguel@gmail.com>
2024-05-27 22:50:45 -0400

    Updates in `biweight_scale` function

--- R/biweight_scale.R
+++ R/biweight_scale.R
@@ -3,13 +3,16 @@
 #' @description
 #' This function computes the biweight scale, a robust measure of scale or dispersion
 #' for a numeric vector. The biweight scale is less sensitive to outliers
-#' than the sample standard deviation. The computation is done iteratively,
-#' following the algorithm described in Mosteller and Tukey (1977).
+#' than the sample standard deviation.
 #'
 #' @param X A numeric vector.
-#' @param loc Location estimate to use (default: biweight location of `X`).
-#' @param tol Convergence tolerance for the iterative computation (default: 1e-6).
-#' @param max_iter Maximum number of iterations (default: 50).
+#' @param loc Location estimate to use (default: median of `X`).
+#' @param c A numeric value specifying the tuning constant for the biweight estimator
+#' (`c = 9` by default).
+#' @param reduced A logical value specifying whether the sample size, *n*,
+#' should be reduced to the number of non-rejected values. If `TRUE`, *n* is
+#' reduced to the number of observations that pass a rejection criteria.
+#' If `FALSE` (default), *n* is equal to length of `X` (the input data).
 #' @return The biweight scale of `X`.
 #'
 #' @references
@@ -18,19 +21,22 @@
 #'
 #' @examples
 #' # Example 1: Compute biweight scale for a vector
-#' x <- c(1, 2, 3, 4, 5)
-#' stats::sd(x)
-#' stats::mad(x)
-#' biweight_scale(x)
+#' x <- c(seq(1,100))
+#' tibble::tibble(
+#' sd = stats::sd(x),
+#' mad = stats::mad(x),
+#' biscale = biweight_scale(x)
+#' )
 #'
 #' # Example 2: Biweight scale is robust to outliers
-#' x <- c(1, 2, 3, 4, 100)  # An outlier at 100
-#' stats::sd(x)
-#' stats::mad(x)
-#' biweight_scale(x)
-#'
+#' x <- c(seq(1,99), 1e3) # An outlier at 1000
+#' tibble::tibble(
+#' sd = stats::sd(x),
+#' mad = stats::mad(x),
+#' biscale = biweight_scale(x)
+#' )
 #' @export biweight_scale
-biweight_scale <- function(X, loc = biweight_location(X), tol = 1e-6, max_iter = 50) {
+biweight_scale <- function(X, loc = stats::median(X), c = 9, reduced = FALSE) {
   if (missing(X)) {
     stop("Input 'X' must be provided.")
   }
@@ -43,20 +49,26 @@ biweight_scale <- function(X, loc = biweight_location(X), tol = 1e-6, max_iter =
   if (length(X) < 2) {
     stop("'X' must have at least two elements.")
   }
-
-  biweight_scale <- stats::mad(X, center = loc)
-
-  for (iter in 1:max_iter) {
-    u <- (X - loc) / (9 * stats::qnorm(0.75) * biweight_scale)
-    w <- dplyr::if_else(abs(u) <= 1, (1 - u^2)^2, 0)
-    new_biweight_scale <- stats::mad(X * w, center = 0, constant = 1) / stats::mad(w, center = 0, constant = 1)
-
-    if (abs(new_biweight_scale - biweight_scale) < tol) {
-      break
-    }
-
-    biweight_scale <- new_biweight_scale
+  if (!is.numeric(c)) {
+    stop("'c' must be numeric.")
+  }
+  if (!is.logical(reduced)) {
+    stop("'reduced' must be a logical value (TRUE or FALSE).")
   }
 
-  return(biweight_scale)
+  mad_x <- stats::mad(X, center = loc)
+
+  if (mad_x == 0) {
+    biscale <- 0
+  } else {
+    u <- (X - loc) / (c * mad_x)
+    u_mask <- abs(u) < 1
+
+    p <- sum((X[u_mask] - loc)^2 * (1 - u[u_mask]^2)^4)
+    q <- sum((1 - u[u_mask]^2)^2 * (1 - 5 * u[u_mask]^2))
+
+    n <- dplyr::if_else(reduced == FALSE, length(X), length(u_mask))
+    biscale <- sqrt(n) * (sqrt(p) / abs(q))
+  }
+  return(biscale)
 }
