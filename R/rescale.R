d6c32eebb67cbe5c9e9fc2974287146ee12e6ce6
ChristianGoueguel <christian.goueguel@gmail.com>
2024-06-08 15:11:24 -0400

    Updates

---
+++ R/rescale.R
@@ -0,0 +1,44 @@
+#' @title Rescaling Numeric Vector Using Min-Max Normalization
+#'
+#' @author Christian L. Goueguel
+#'
+#' @description
+#' This function rescales a numeric vector using the min-max normalization technique,
+#' which linearly transforms the values to a new range specified by the `a` and
+#' `b` arguments. The minimum value in the original vector is mapped to `a`, and
+#' the maximum value is mapped to `b`.
+#'
+#' @param x A numeric vector.
+#' @param a The minimum value of the new range (default: 0).
+#' @param b The maximum value of the new range (default: 1).
+#'
+#' @return A numeric vector of the same length as `x`, with values rescaled to
+#' the new range `[a, b]`.
+#'
+#' @export rescale
+#'
+rescale <- function(x, a = 0, b = 1) {
+  if (missing(x)) {
+    stop("Missing 'x' argument.")
+  }
+  if (!is.numeric(x)) {
+    stop("'x' must be a numeric vector.")
+  }
+  if (!is.numeric(a) || !is.numeric(b)) {
+    stop("'a' and 'b' must be numeric.")
+  }
+  if (b <= a) {
+    stop("'b' must be greater than 'a'.")
+  }
+
+  x_min <- min(x, na.rm = TRUE)
+  x_max <- max(x, na.rm = TRUE)
+
+  if (x_min == x_max) {
+    return(rep(a, length(x)))
+  }
+
+  x_rescaled <- a + ((x - x_min) * (b - a)) / (x_max - x_min)
+
+  return(x_rescaled)
+}
